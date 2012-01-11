af10630ef3e092f9da4be1680b930e65c68b292f
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-01-11 17:10:26 -0500

    added LICENSE (MPL 2.0)

--- Rawbots/Program.cs
+++ Rawbots/Program.cs
@@ -1,4 +1,13 @@
-﻿// Released to the public domain. Use, modify and relicense at will.
+﻿/**
+ * RawBots: an awesome robot game
+ * 
+ * Copyright 2012 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ * Copyright 2012 Mark Foo Bonasoro <foo_mark@q8ismobile.com>
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 using System;
 
@@ -13,15 +22,12 @@ namespace StarterKit
 {
 	class Game : GameWindow
 	{
-		/// <summary>Creates a 800x600 window with the specified title.</summary>
 		public Game()
 			: base(800, 600, GraphicsMode.Default, "OpenTK Quick Start Sample")
 		{
 			VSync = VSyncMode.On;
 		}
 
-		/// <summary>Load resources here.</summary>
-		/// <param name="e">Not used.</param>
 		protected override void OnLoad(EventArgs e)
 		{
 			base.OnLoad(e);
@@ -30,27 +36,17 @@ namespace StarterKit
 			GL.Enable(EnableCap.DepthTest);
 		}
 
-		/// <summary>
-		/// Called when your window is resized. Set your viewport here. It is also
-		/// a good place to set up your projection matrix (which probably changes
-		/// along when the aspect ratio of your window).
-		/// </summary>
-		/// <param name="e">Not used.</param>
 		protected override void OnResize(EventArgs e)
 		{
 			base.OnResize(e);
 
 			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
 
-			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float) Math.PI / 4, Width / (float) Height, 1.0f, 64.0f);
 			GL.MatrixMode(MatrixMode.Projection);
 			GL.LoadMatrix(ref projection);
 		}
 
-		/// <summary>
-		/// Called when it is time to setup the next frame. Add you game logic here.
-		/// </summary>
-		/// <param name="e">Contains timing information for framerate independent logic.</param>
 		protected override void OnUpdateFrame(FrameEventArgs e)
 		{
 			base.OnUpdateFrame(e);
@@ -59,10 +55,6 @@ namespace StarterKit
 				Exit();
 		}
 
-		/// <summary>
-		/// Called when it is time to render the next frame. Add your rendering code here.
-		/// </summary>
-		/// <param name="e">Contains timing information.</param>
 		protected override void OnRenderFrame(FrameEventArgs e)
 		{
 			base.OnRenderFrame(e);
@@ -84,15 +76,9 @@ namespace StarterKit
 			SwapBuffers();
 		}
 
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		static void Main()
 		{
-			// The 'using' idiom guarantees proper resource cleanup.
-			// We request 30 UpdateFrame events per second, and unlimited
-			// RenderFrame events (as fast as the computer can handle).
 			using (Game game = new Game())
 			{
 				game.Run(30.0);
