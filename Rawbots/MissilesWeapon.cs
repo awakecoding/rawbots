3f8d0e2a2688ea16a27a8b3187323e949ce5d8d2
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-02-03 19:16:27 -0500

    modeling missiles weapon

--- Rawbots/MissilesWeapon.cs
+++ Rawbots/MissilesWeapon.cs
@@ -17,8 +17,38 @@ namespace Rawbots
 {
 	public class MissilesWeapon : Weapon
 	{
+        private float inCylinderRadius;
+        private float inCylinderHeight;
+        private float outCylinderRadius;
+        private float outCylinderHeight;
+        private float cubeSize;
+        private CylinderModel inCylinder;
+        private CylinderModel outCylinder;
+        private CubeModel middleBox;
+        private CubeModel frame;
+        private TeamNumber teamNumber;
+        //CubeModel box; //For 1unit cubed box
+
 		public MissilesWeapon()
 		{
+            teamNumber = new TeamNumber();
+            
+            inCylinderRadius = 0.1f;
+            inCylinderHeight = 1.0f;
+            inCylinder = new CylinderModel(inCylinderRadius, inCylinderHeight);
+            inCylinder.SetColor(0.4f, 0.5f, 0.6f);
+
+            outCylinderRadius = 0.15f;
+            outCylinderHeight = 0.3f;
+            outCylinder = new CylinderModel(outCylinderRadius, outCylinderHeight);
+            outCylinder.SetColor(0.0f, 0.7f, 0.6f);
+
+            cubeSize = 1.0f; //Size of standard cube
+            middleBox = new CubeModel();
+            middleBox.SetColor(0.1f, 0.3f, 0.3f);
+
+            frame = new CubeModel();
+            frame.SetColor(0.1f, 1.0f, 0.0f);
 		}
 		
 		public override int getCost()
@@ -38,16 +68,85 @@ namespace Rawbots
 
 		public override void Render()
 		{
-			GL.Begin(BeginMode.Triangles);
+            //left missle chamber
+            GL.PushMatrix();
+            GL.Translate(-0.35f, 0.0f, 0.0f);
+            //inner chamber
+            GL.PushMatrix();
+            GL.Translate(0.0f, 0.0f, -0.5f);
+            inCylinder.render();
+            GL.PopMatrix();
 
-			GL.Color3(1.0f, 1.0f, 0.0f);
-			GL.Vertex3(-1.0f, -1.0f, 4.0f);
-			GL.Color3(1.0f, 0.0f, 0.0f);
-			GL.Vertex3(1.0f, -1.0f, 4.0f);
-			GL.Color3(0.2f, 0.9f, 1.0f);
-			GL.Vertex3(0.0f, 1.0f, 4.0f);
-			
-			GL.End();
+            //outer chambers
+                //back chamber
+                GL.PushMatrix();
+                GL.Translate(0.0f, 0.0f, -0.4f);
+                outCylinder.render();
+                GL.PopMatrix();
+                //front chamber
+                GL.PushMatrix();
+                GL.Translate(0.0f, 0.0f, 0.1f);
+                outCylinder.render();
+                GL.PopMatrix();
+            GL.PopMatrix();
+
+            //right missle chamber
+            GL.PushMatrix();
+            GL.Translate(0.35f, 0.0f, 0.0f);
+            //inner chamber
+            GL.PushMatrix();
+            GL.Translate(0.0f, 0.0f, -0.5f);
+            inCylinder.render();
+            GL.PopMatrix();
+
+            //outer chambers
+                //back chamber
+                GL.PushMatrix();
+                GL.Translate(0.0f, 0.0f, -0.4f);
+                outCylinder.render();
+                GL.PopMatrix();
+                //front chamber
+                GL.PushMatrix();
+                GL.Translate(0.0f, 0.0f, 0.1f);
+                outCylinder.render();
+                GL.PopMatrix();
+            GL.PopMatrix();
+
+            //Middle cube creation
+            GL.PushMatrix();
+            GL.Scale(0.5f, 0.4f, 0.5f);
+            middleBox.render(cubeSize);
+            GL.PopMatrix();
+
+            //Top frame
+            GL.PushMatrix();
+            GL.Translate(0.0f, 0.2f, 0.0f);
+            GL.Scale(0.7f, 0.1f, 0.7f);
+            frame.render(cubeSize);
+            GL.PopMatrix();
+
+            //Bottom frame
+            GL.PushMatrix();
+            GL.Translate(0.0f, -0.2f, 0.0f);
+            GL.Scale(0.7f, 0.1f, 0.7f);
+            frame.render(cubeSize);
+            GL.PopMatrix();
+
+            GL.PushMatrix();
+            GL.Translate(0.0f, -0.2f, 0.0f);
+            teamNumber.render();
+            GL.PopMatrix();
+
+            //WIRE FRAME BOX - to make sure it fits in one unit cube.
+            /*
+            GL.PushMatrix();
+            box = new CubeModel();
+
+            box.setColor(1f, 1.0f, 1.0f);
+            box.setRenderMode(Model.RenderMode.WIRE);
+            box.render(1);
+            GL.PopMatrix();
+            */
 		}
 	}
 }
