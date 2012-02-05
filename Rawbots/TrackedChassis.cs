19c2bd2b6b9e1db26c9155c07772c3d68b6ccce0
Astou Sene <Chanel@dyn-240-192.wireless.concordia.ca>
2012-02-05 18:09:57 -0500

    Implement TrackedChassis code

--- Rawbots/TrackedChassis.cs
+++ Rawbots/TrackedChassis.cs
@@ -17,12 +17,28 @@ namespace Rawbots
 {
 	public class TrackedChassis : Chassis
 	{
+		
+		private ModelCube[] tcComponent;
+		private ModelCylinder[] wheelEnd;
+		private const int bottomEnd = 0;
+		private const int topEnd = 1;
+		private const int TOTAL_ENDS = 2;
+		
 		public TrackedChassis()
 		{
 			/* 
 			 * Considerably more manoeuvrable than
 			 * bipods but twice the resource units.
 			 */
+			
+			wheelEnd = new ModelCylinder[TOTAL_ENDS];
+            for (int i = 0; i < TOTAL_ENDS; i++)
+                 wheelEnd[i] = new ModelCylinder();
+			
+			tcComponent = new ModelCube[TOTAL_ENDS];
+            for (int i = 0; i < TOTAL_ENDS; i++)
+                 tcComponent[i] = new ModelCube();
+
 		}
 
         public override void setRenderMode(int mode)
@@ -32,7 +48,7 @@ namespace Rawbots
 
 		public override void Render()
 		{
-			GL.Begin(BeginMode.Triangles);
+			/*GL.Begin(BeginMode.Triangles);
 
 			GL.Color3(1.0f, 1.0f, 0.0f);
 			GL.Vertex3(-1.0f, -1.0f, 4.0f);
@@ -41,8 +57,55 @@ namespace Rawbots
 			GL.Color3(0.2f, 0.9f, 1.0f);
 			GL.Vertex3(0.0f, 1.0f, 4.0f);
 			
-			GL.End();
+			GL.End();*/
+			
+			GL.PushMatrix();
+			
+			GL.Scale(0.5f, 0.5f, 1.0f);
+			
+			tcComponent[topEnd].render(1.0f);
+			
+			GL.PopMatrix ();
+			GL.PushMatrix();
+			
+			GL.Translate(-0.5f, 0.0f, 0.0f);
+			
+			drawWheel();
+			
+			GL.PopMatrix();
+			GL.PushMatrix();
+			
+			GL.Translate(0.5f, 0.0f, 0.0f);
+			
+			drawWheel();
+			
+			GL.PopMatrix();
 		}
+		
+		public void drawWheel()
+		{
+			GL.PushMatrix();
+			
+			GL.Translate(0.0f, 0.0f,1.0f);
+			
+			wheelEnd[topEnd].render(0.5, 1.0, 8, 1);
+			
+			GL.PopMatrix();
+			GL.PushMatrix();
+			
+			GL.Translate (0.0f, 0.0f, -1.0f);
+			
+			wheelEnd[bottomEnd].render(0.5, 1.0,  8, 1);
+			
+			GL.PopMatrix();
+			GL.PushMatrix();
+			
+			GL.Scale(0.25f, 1.0f, 1.0f);
+			
+			tcComponent[bottomEnd].render(1.0f);
+			
+			GL.PopMatrix();
+		}
+			
 	}
 }
-
