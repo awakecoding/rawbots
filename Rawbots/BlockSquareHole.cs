befa951d6d1bc87581de6b0f20e9dea7c308c1e6
foomark <foo_mark@q8ismobile.com>
2012-02-05 19:24:29 -0500

    Added Block buildings with holes.

---
+++ Rawbots/BlockSquareHole.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+
+namespace Rawbots
+{
+    class BlockSquareHole : Block
+    {
+        public BlockSquareHole(bool half) : base(half)
+        {}
+
+        public BlockSquareHole(bool half, int x, int y)
+            : base(half, x, y)
+        {}
+
+        public override void Render()
+        {
+            base.Render();
+
+            GL.PushMatrix();
+
+            getCube().SetColor(0.0f, 0.0f, 0.0f);
+
+            
+
+            if (this.isHalf())
+            {
+                GL.Translate(0.0f, 0.25f, 0.0f);
+                GL.Scale(0.75f, 0.5f, 0.75f);
+                GL.Scale(1.0f, 0.5f, 1.0f);
+                GL.Translate(0.0f, 0.5f, 0.0f);
+            }
+            else
+            {
+                GL.Translate(0.0f, 0.75f, 0.0f);
+                GL.Scale(0.75f, 0.5f, 0.75f);
+                GL.Translate(0.0f, 0.5f, 0.0f);
+            }
+
+            getCube().render(1.0f);
+
+            GL.PopMatrix();
+        }
+
+    }
+}
