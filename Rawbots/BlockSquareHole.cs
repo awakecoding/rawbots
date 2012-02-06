0dec82e8b57a5969ccc65474fc3b8603a9c57a1c
unknown <foo_mark@foo_mark-PC.(none)>
2012-02-06 00:58:03 -0500

    Adjusted the height for the hole to render.

--- Rawbots/BlockSquareHole.cs
+++ Rawbots/BlockSquareHole.cs
@@ -26,7 +26,7 @@ namespace Rawbots
 
             if (this.isHalf())
             {
-                GL.Translate(0.0f, 0.25f, 0.0f);
+                GL.Translate(0.0f, 0.26f, 0.0f);
                 GL.Scale(0.75f, 0.5f, 0.75f);
                 GL.Scale(1.0f, 0.5f, 1.0f);
                 GL.Translate(0.0f, 0.5f, 0.0f);
