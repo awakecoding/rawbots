fbbf24ed7cd69f90ad3c5c25a2b9630869eda8e3
foomark <foo_mark@q8ismobile.com>
2012-02-25 11:08:39 -0500

    Fixed SphereModel. Lighting was not correct due to the fact that there were no normals on the surface, hence you get a very flat color instead an interpolated smooth surface.

--- Rawbots/Model/SphereModel.cs
+++ Rawbots/Model/SphereModel.cs
@@ -83,25 +83,167 @@ namespace Rawbots
 			GL.PopMatrix();
 		}
 
+		public void renderSolidSphere(double radius, int slices, int stacks)
+		{ 
+			int i,j;
+
+			/* Adjust z and radius as stacks are drawn. */
+
+			double z0,z1;
+			double r0,r1;
+
+			if (sint == null || cost == null)
+				CircleTable(ref sint, ref cost, -slices);
+			else if (sint != null || cost != null)
+				if (sint.Length != Math.Abs(slices) + 1)
+					CircleTable(ref sint, ref cost, -slices);
+
+			if (sint2 == null || cost2 == null)
+				CircleTable(ref sint2, ref cost2, stacks * 2);
+			else if (sint2 != null || cost2 != null)
+				if(sint2.Length != Math.Abs(slices) + 1)
+					CircleTable(ref sint2, ref cost2, stacks * 2);
+
+			/* The top stack is covered with a triangle fan */
+
+			z0 = 1.0;
+			z1 = cost2[(stacks>0)?1:0];
+			r0 = 0.0;
+			r1 = sint2[(stacks>0)?1:0];
+
+			GL.Begin(BeginMode.TriangleFan);
+
+				GL.Normal3(0.0f,0.0f,1.0f);
+				GL.Vertex3(0.0f,0.0f,radius);
+
+				for (j=slices; j>=0; j--)
+				{
+					GL.Normal3(cost[j]*r1,        sint[j]*r1,        z1       );
+					GL.Vertex3(cost[j]*r1*radius, sint[j]*r1*radius, z1*radius);
+				}
+
+			GL.End();
+
+			/* Cover each stack with a quad strip, except the top and bottom stacks */
+
+			for( i=1; i<stacks-1; i++ )
+			{
+				z0 = z1; z1 = cost2[i+1];
+				r0 = r1; r1 = sint2[i+1];
+
+				GL.Begin(BeginMode.QuadStrip);
+
+					for(j=0; j<=slices; j++)
+					{
+						GL.Normal3(cost[j]*r1,        sint[j]*r1,        z1       );
+						GL.Vertex3(cost[j]*r1*radius, sint[j]*r1*radius, z1*radius);
+						GL.Normal3(cost[j]*r0,        sint[j]*r0,        z0       );
+						GL.Vertex3(cost[j]*r0*radius, sint[j]*r0*radius, z0*radius);
+					}
+
+				GL.End();
+			}
+
+			/* The bottom stack is covered with a triangle fan */
+
+			z0 = z1;
+			r0 = r1;
+
+			GL.Begin(BeginMode.TriangleFan);
+
+				GL.Normal3(0.0f,0.0f,-1.0f);
+				GL.Vertex3(0.0f,0.0f,-radius);
+
+				for (j=0; j<=slices; j++)
+				{
+					GL.Normal3(cost[j]*r0,        sint[j]*r0,        z0       );
+					GL.Vertex3(cost[j]*r0*radius, sint[j]*r0*radius, z0*radius);
+				}
+
+			GL.End();
+		}
+
+		public void renderWireSphere(double radius, int slices, int stacks)
+		{ 
+			int i,j;
+
+			/* Adjust z and radius as stacks and slices are drawn. */
+
+			double r;
+			double x,y,z;
+
+			if (sint == null || cost == null)
+				CircleTable(ref sint, ref cost, -slices);
+			else if (sint != null || cost != null)
+				if (sint.Length != Math.Abs(slices) + 1)
+					CircleTable(ref sint, ref cost, -slices);
+
+			if (sint2 == null || cost2 == null)
+				CircleTable(ref sint2, ref cost2, stacks * 2);
+			else if (sint2 != null || cost2 != null)
+				if (sint2.Length != Math.Abs(slices) + 1)
+					CircleTable(ref sint2, ref cost2, stacks * 2);
+
+			/* Draw a line loop for each stack */
+
+			for (i=1; i<stacks; i++)
+			{
+				z = cost2[i];
+				r = sint2[i];
+
+				GL.Begin(BeginMode.LineLoop);
+
+					for(j=0; j<=slices; j++)
+					{
+						x = cost[j];
+						y = sint[j];
+
+						GL.Normal3(x,y,z);
+						GL.Vertex3(x*r*radius,y*r*radius,z*radius);
+					}
+
+				GL.End();
+			}
+
+			/* Draw a line loop for each slice */
+
+			for (i=0; i<slices; i++)
+			{
+				GL.Begin(BeginMode.LineStrip);
+
+					for(j=0; j<=stacks; j++)
+					{
+						x = cost[i]*sint2[j];
+						y = sint[i]*sint2[j];
+						z = cost2[j];
+
+						GL.Normal3(x,y,z);
+						GL.Vertex3(x*radius,y*radius,z*radius);
+					}
+
+				GL.End();
+			}
+		}
+
         public virtual void render()
         {
             switch (renderMode)
             {
                 case RenderMode.SOLID:
                     GL.Color3(colorR, colorG, colorB);
-    				renderSphere(true);
-                	break;
+					renderSolidSphere(Radius, LongitudinalSlices, LatitudinalSlices);
+					break;
 				
                 case RenderMode.WIRE:
 					GL.Color3(colorR, colorG, colorB);
-                    renderSphere(false);
-					break;
+					renderWireSphere(Radius, LongitudinalSlices, LatitudinalSlices);
+                    break;
 				
 				case RenderMode.SOLID_WIRE:
 					GL.Color3(colorR, colorG, colorB);
-                    renderSphere(true);
+					renderSolidSphere(Radius, LongitudinalSlices, LatitudinalSlices);
 					GL.Color3(wireColorR, wireColorG, wireColorB);
-                    renderSphere(false);
+					renderWireSphere(Radius, LongitudinalSlices, LatitudinalSlices);
 					break;
             }
         }
