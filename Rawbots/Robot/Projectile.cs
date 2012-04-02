cbd3bd5457cb34cf5a40336cf7938ee5978112e8
foomark <foo_mark@q8ismobile.com>
2012-04-02 18:24:10 -0400

    Forgot to add the Projectile class file.

---
+++ Rawbots/Robot/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rawbots
+{
+	class Projectile : Drawable
+	{
+		const int LIFE_TIME = 1000;
+		int lifeTime = LIFE_TIME; //The time of life the projectile is in function till it dies or gets hit
+
+		SphereModel flare = new SphereModel(1.0f);
+
+		public Projectile()
+		{
+			flare.SetColor(0.75f, 0.0f, 0.0f);
+			flare.SetRenderMode(RenderMode.SOLID);
+		}
+
+		public override void Render()
+		{
+			flare.render();
+		}
+	}
+}
